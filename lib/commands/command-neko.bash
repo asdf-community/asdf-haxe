e3642752e312c82c4c85ea6326030eb68661843c
Thiago Costa <thiago.costa@groupon.com>
2020-07-30 10:08:31 -0700

    modify bash-commands to support other OS dylibs

--- lib/commands/command-neko.bash
+++ lib/commands/command-neko.bash
@@ -51,25 +51,53 @@ dylibs_link() {
   local haxe_path neko_path
   haxe_path=$(asdf where haxe)
   neko_path=$(asdf where neko)
+  dylib_ext="dylib"
+  case "$OSTYPE" in
+    solaris*) dylib_ext="so"    ;;
+    darwin*)  dylib_ext="dylib" ;;
+    linux*)   dylib_ext="so"    ;;
+    bsd*)     dylib_ext="so"    ;;
+    msys*)    dylib_ext="dll"   ;;
+    *)        dylib_ext="dylib" ;;
+  esac
   find "$neko_path/bin" -mindepth 1 -maxdepth 1 \
-    -name "*.dylib" \
+    -name "*.$dylib_ext" \
     -exec ln -sfv {} "$haxe_path/bin" \;
 }
 
 dylibs_unlink() {
   local haxe_path
   haxe_path=$(asdf where haxe)
+  dylib_ext="dylib"
+  case "$OSTYPE" in
+    solaris*) dylib_ext="so"    ;;
+    darwin*)  dylib_ext="dylib" ;;
+    linux*)   dylib_ext="so"    ;;
+    bsd*)     dylib_ext="so"    ;;
+    msys*)    dylib_ext="dll"   ;;
+    *)        dylib_ext="dylib" ;;
+  esac
   find "$haxe_path/bin" -mindepth 1 -maxdepth 1 \
-    -type l -path "*neko*.dylib" \
+    -type l -path "*neko*.$dylib_ext" \
     -exec rm -v {} \;
 }
 
 dylibs_which() {
+  set -x
   local haxe_path
   haxe_path=$(asdf where haxe)
+  dylib_ext="dylib"
+  case "$OSTYPE" in
+    solaris*) dylib_ext="so"    ;;
+    darwin*)  dylib_ext="dylib" ;;
+    linux*)   dylib_ext="so"    ;;
+    bsd*)     dylib_ext="so"    ;;
+    msys*)    dylib_ext="dll"   ;;
+    *)        dylib_ext="dylib" ;;
+  esac
   find "$haxe_path/bin" -mindepth 1 -maxdepth 1 \
-    -type l -path "*neko*.dylib" | head -n 1 |
-    xargs dirname | xargs dirname | xargs basename
+    -type l -path "*neko*.$dylib_ext" \
+    -exec sh -c 'dirname {} | xargs dirname | xargs basename' \; -quit
 }
 
 case "$*" in
